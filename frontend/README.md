e72e6d75a46420c8ed5696ccd6ed4669dd67b632
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-09-16 16:20:46 +0530

    setup

---
+++ frontend/README.md
@@ -0,0 +1,4 @@
+```bash
+npm install @solana/web3.js @solana/wallet-adapter-react @solana/wallet-adapter-base @solana/wallet-adapter-react-ui @solana/wallet-adapter-wallets
+npm install react react-dom buffer
+```
---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Buffer } from 'buffer';
+import App from './App.tsx'
+import './index.css'
+globalThis.Buffer = Buffer;
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
